93fa367635af7d8559fae18746f8a9c66c6d525a
Sarah Lee <sarahhhh.lee@mail.utoronto.ca>
2024-09-26 02:27:00 -0400

    update scripts/01-download_data.R

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,35 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from opendatatoronto
+# Author: Sarah Lee
+# Date: 24 September 2024 
+# Contact: sarahhhh.lee@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# get package
+package <- show_package("dogs-issued-dangerous-dog-orders")
+package
 
+# get all resources for this package
+resources <- list_package_resources("dogs-issued-dangerous-dog-orders")
 
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# load the first datastore resource as a sample
+data <- filter(datastore_resources, row_number()==1) %>% get_resource()
+data
 
 #### Save data ####
-# [...UPDATE THIS...]
+#to save where I want the file to be saved
+folder_path <- "/Users/sarahlee/Documents/STA304/dangerous_dogs/dangerous_dogs/data"  # Change this to your desired folder path
+
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(data, file.path(folder_path, "dangerous_dog_raw.csv")) 
 
 
