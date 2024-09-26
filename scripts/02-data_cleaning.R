e1fb787f7a12b4c5b3e2d0a25dadb0bef7098961
Sarah Lee <sarahhhh.lee@mail.utoronto.ca>
2024-09-26 02:26:43 -0400

    update scripts/02-data_cleaning.R

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,8 +1,8 @@
 #### Preamble ####
 # Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Sarah Lee
+# Date: 24 September 2024 
+# Contact: sarahhhh.lee@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
@@ -11,34 +11,26 @@
 library(tidyverse)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_data <- read_csv("/Users/sarahlee/Documents/STA304/dangerous_dogs/dangerous_dogs/data/dangerous_dog_raw.csv")
 
-cleaned_data <-
+cleaned_data <- 
   raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
   mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                          "1.35",
-                                          flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-  ) |> 
-  tidyr::drop_na()
+    year = year(Date_of_Dangerous_Act),
+    month = month(Date_of_Dangerous_Act),
+    day = day(Date_of_Dangerous_Act) #separating the data into year, month, and day respectively
+    ) |>
+  rename(
+    geographic_area = Forward_Sortation_Area,
+    dog_name = Name_of_Dog,
+    ward = Ward_Name, 
+    bite_type = Bite_Circumstance,
+    location_type = Location_of_Incident #rename some of the coloumn names
+    ) |>
+  mutate(bite_type = ifelse(bite_type == "NAB", "NOT A BITE", bite_type) #Change rows in bite_type to make sure it has the same words
+    ) |>
+  select(year, month, day, Breed, Color, ward, bite_type, location_type, geographic_area, dog_name) #pick out columns that will be used
+ print(cleaned_data)
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(cleaned_data, "/Users/sarahlee/Documents/STA304/dangerous_dogs/dangerous_dogs/data/dangerous_dog_clean.csv")
