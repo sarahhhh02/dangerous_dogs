97185e2868e17bf49d64f30f5536cebc4eba02ba
Sarah Lee <sarahhhh.lee@mail.utoronto.ca>
2024-09-26 23:37:37 -0400

    update scripts/02-data_cleaning.R

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,18 +1,19 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
+# Purpose: Cleans the raw dangerous dog incident dataset
 # Author: Sarah Lee
 # Date: 24 September 2024 
 # Contact: sarahhhh.lee@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+
 
 #### Workspace setup ####
 library(tidyverse)
 
 #### Clean data ####
+# Call the saved raw data
 raw_data <- read_csv("/Users/sarahlee/Documents/STA304/dangerous_dogs/dangerous_dogs/data/dangerous_dog_raw.csv")
 
+# Cleaning the raw data
 cleaned_data <- 
   raw_data |>
   mutate(
@@ -25,12 +26,11 @@ cleaned_data <-
     dog_name = Name_of_Dog,
     ward = Ward_Name, 
     bite_type = Bite_Circumstance,
-    location_type = Location_of_Incident #rename some of the coloumn names
+    location_type = Location_of_Incident #rename some of the column names
     ) |>
   mutate(bite_type = ifelse(bite_type == "NAB", "NOT A BITE", bite_type) #Change rows in bite_type to make sure it has the same words
     ) |>
   select(year, month, day, Breed, ward, bite_type, location_type, geographic_area) #pick out columns that will be used
- print(cleaned_data)
 
 #### Save data ####
 write_csv(cleaned_data, "/Users/sarahlee/Documents/STA304/dangerous_dogs/dangerous_dogs/data/dangerous_dog_clean.csv")
